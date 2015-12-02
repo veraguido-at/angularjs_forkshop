81dd0e850156c3e6a525e2ecb9756d3e74225e3b
mliwski <matias.liwski@avantrip.com>
2015-12-02 16:47:34 -0300

    Implemented CRUD on service

--- src/components/tasks/tasksService.js
+++ src/components/tasks/tasksService.js
@@ -9,7 +9,7 @@
 
             return {
                 create: function(task){
-                    task['status'] = 'WIP';
+                    task.status = 'WIP';
                     var request = {
                         method: 'POST',
                         url: base_uri,
@@ -33,6 +33,44 @@
                         headers: {
                             'Content-Type': 'application/json'
                         },
+                        params: {
+                            'apiKey': api_key
+                        }
+                    };
+                    if(filters){
+
+                    }
+
+                    return $http(request).then(function (response) {
+                        return response.data;
+                    });
+                },
+                update: function(task){
+                    var taskId = task._id.$oid;
+                    delete task._id;
+                    var request = {
+                        method: 'PUT',
+                        url: base_uri+'/'+taskId,
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        param: {
+                            'apiKey': api_key
+                        },
+                        data: task
+                    };
+
+                    return $http(request).then(function (response) {
+                        return response.data;
+                    });
+                },
+                delete: function(taskId){
+                    var request = {
+                        method: 'DELETE',
+                        url: base_uri+'/'+taskId,
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
                         param: {
                             'apiKey': api_key
                         }
@@ -41,12 +79,6 @@
                     return $http(request).then(function (response) {
                         return response.data;
                     });
-                },
-                update: function(task){
-
-                },
-                delete: function(taskId){
-
                 }
 
             };
--- test/unit/tasks/tasksServiceSpecs.js
+++ test/unit/tasks/tasksServiceSpecs.js
@@ -21,7 +21,8 @@
             });
         });
 
-        it('Should translate read service to httpGet base_uri', inject(function(Tasks, $httpBackend) {
+        //Translation asserts
+        /*it('Should translate read service to httpGet base_uri', inject(function(Tasks, $httpBackend) {
             $httpBackend.when('GET', configMock.mongolab.base_uri)
                 .respond([ { "_id" : { "$oid" : "565c94a8e4b03d453c995e48"} , "description" : "Hacer el workshop" , "status" : "WIP"} ]);
 
@@ -30,7 +31,7 @@
             });
 
             $httpBackend.flush();
-        }));
+        }));*/
 
         it('Should translate create service to httpPost', inject(function(Tasks, $httpBackend) {
             var taskToCreate = {"description" : "Hacer el commit inicial"};
@@ -45,6 +46,33 @@
             $httpBackend.flush();
         }));
 
+        it('Should translate delete service to httpDelete', inject(function(Tasks, $httpBackend){
+            var taskIdToDelete = "565c94a8e4b03d453c995e48";
+
+            $httpBackend.when('DELETE', configMock.mongolab.base_uri+"/"+taskIdToDelete)
+                .respond({ "_id" : { "$oid" : taskIdToDelete} , "description" : "Hacer el workshop" , "status" : "WIP"});
+
+            Tasks.delete(taskIdToDelete).then(function(deletedTask){
+                expect(deletedTask._id).not.toBeNull();
+            });
+
+            $httpBackend.flush();
+        }));
+
+        it('Should translate update service to httpPut', inject(function(Tasks, $httpBackend){
+            var taskToUpdate = { "_id" : { "$oid" : "565c94a8e4b03d453c995e48"} , "description" : "Hacer el Commit inicial" , "status" : "WIP"};
+
+            $httpBackend.when('PUT', configMock.mongolab.base_uri+"/"+taskToUpdate._id.$oid)
+                .respond({ "_id" : { "$oid" : "565c94a8e4b03d453c995e48"} , "description" : "Hacer el Commit inicial" , "status" : "WIP"});
+
+            Tasks.update(taskToUpdate).then(function(updatedTask){
+                expect(updatedTask._id).not.toBeNull();
+            });
+
+            $httpBackend.flush();
+        }));
+
+        //Functionality asserts
         it('Should apply status WIP to all new tasks', inject(function(Tasks, $httpBackend) {
             var taskToCreate = {"description" : "Hacer el commit inicial"};
 
@@ -54,8 +82,21 @@
             Tasks.create(taskToCreate);
 
             var taskWithWIP = _.clone(taskToCreate);
-            taskWithWIP['status'] = 'WIP';
+            taskWithWIP.status = 'WIP';
             $httpBackend.expectPOST(configMock.mongolab.base_uri, taskWithWIP);
+            $httpBackend.flush();
+        }));
+
+        it('Should translate delete service to httpDelete', inject(function(Tasks, $httpBackend){
+            var taskIdToDelete = "565c94a8e4b03d453c995e48";
+
+            $httpBackend.when('DELETE', configMock.mongolab.base_uri+"/"+taskIdToDelete)
+                .respond([ { "_id" : { "$oid" : taskIdToDelete} , "description" : "Hacer el workshop" , "status" : "WIP"} ]);
+
+            Tasks.delete(taskIdToDelete).then(function(deletedTask){
+                expect(deletedTask._id).not.toBeNull();
+            });
+
             $httpBackend.flush();
         }));
 
